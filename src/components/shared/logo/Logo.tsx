afe2e0bfc9a49f7e730036d9eaa0e168937c1bf5
tahsin0909 <tajisan678@gmail.com>
2025-01-14 11:18:44 +0600

    updated2.0

--- src/components/shared/logo/Logo.tsx
+++ src/components/shared/logo/Logo.tsx
@@ -2,7 +2,7 @@ import logo from "@/assets/logo/logo.jpeg"
 const Logo = () => {
     return (
         <div>
-            <img src={logo} alt='Brand Logo' width={100} height={100} className='w-[80px] h-[74px]' />
+            <img src={logo} alt='Brand Logo' width={100} height={100} className='w-[150px] h-[120px]' />
         </div>
     );
 };
