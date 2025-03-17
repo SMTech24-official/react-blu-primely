ddb01c3e603b11cd85e05b3b5a315b3bcb3b33c3
tahsin0909 <tajisan678@gmail.com>
2025-03-17 21:44:07 +0600

    updated baseapi

--- src/redux/apis/baseApi.ts
+++ src/redux/apis/baseApi.ts
@@ -5,7 +5,7 @@ import Cookies from "js-cookie";
 export const baseApi = createApi({
   reducerPath: "baseApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://oggt-cxcv-backend.vercel.app", // Your backend base URL
+    baseUrl: "https://oggt-cxcv-backend.vercel.app/api/v1", // Your backend base URL
     // baseUrl: "http://10.0.10.41:5000/api/v1", // Your backend base URL
     credentials: "include",
     prepareHeaders: (headers) => {
